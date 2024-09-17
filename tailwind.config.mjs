28081052f82f940585fdb920239a0216755d370a
laba0002 <laba0002@stud.kea.dk>
2024-09-17 11:32:17 +0200

    border

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-<<<<<<< HEAD
-export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {},
-=======
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
@@ -19,7 +13,6 @@ module.exports = {
         scroll: "scroll 20s linear infinite",
       },
     },
->>>>>>> master
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms"), require("@tailwindcss/aspect-ratio"), require("@tailwindcss/container-queries")],
 };
